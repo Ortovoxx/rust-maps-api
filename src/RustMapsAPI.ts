93a96836b793d6010530b8c744797a5ef8bfe686
Euan <56805259+Ortovoxx@users.noreply.github.com>
2021-02-21 17:09:34 +0000

    Constructor

--- src/RustMapsAPI.ts
+++ src/RustMapsAPI.ts
@@ -6,3 +6,15 @@ import IMapOptions from './structures/MapOptions';
 import IGetMap from './structures/GetMapResponse';
 import IGetFilter from './structures/GetFilterResponse';
 import IGenerateMap from './structures/GenerateMapResponse';
+
+export default class RustMapsAPI {
+    public readonly version: string;
+    public readonly baseurl: string;
+    public readonly apikey: string;
+
+    constructor(apikey: string) {
+        this.version = 'v2';
+        this.baseurl = `https://rustmaps.com/api/${this.version}/maps/`;
+        if (!apikey) throw new Error('You must provide an API key. Login here https://rustmaps.com/user/profile to obtain one');
+        this.apikey = apikey;
+    }
