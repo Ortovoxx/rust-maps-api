bf1ecf483e883a40118522358031c659429174fd
Euan <56805259+Ortovoxx@users.noreply.github.com>
2021-02-21 17:09:16 +0000

    Imports

---
+++ src/RustMapsAPI.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+import { version, name } from '../package.json';
+
+import IMapOptions from './structures/MapOptions';
+import IGetMap from './structures/GetMapResponse';
+import IGetFilter from './structures/GetFilterResponse';
+import IGenerateMap from './structures/GenerateMapResponse';
